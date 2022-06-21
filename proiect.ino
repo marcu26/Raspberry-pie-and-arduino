15e6f3c77d777786f78da5b037107415123f2b99
marcu26 <100121658+marcu26@users.noreply.github.com>
2022-06-21 12:47:51 +0300

    proiect var2

--- proiect.ino
+++ proiect.ino
@@ -6,9 +6,11 @@ Servo capRobot;
 
 #define echoPin 2 
 #define trigPin 3
-#define greenLed 9
-#define redLed 10
+#define greenLed 10
+#define redLed 9
 #define servoPin 5
+#define buttonPin 7
+const int micPin  = A0;
 
 int RECV_PIN=12; 
 IRrecv irrecv(RECV_PIN); 
@@ -20,6 +22,8 @@ int angle = 90;
 bool way=0;
 String line1;
 String line2;
+int option=0;
+int buttonState = 0;  
 
 
 
@@ -30,9 +34,11 @@ void setup(){
   pinMode(echoPin, INPUT);
   pinMode(greenLed, OUTPUT);
   pinMode(redLed, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(micPin, INPUT);
 
-  digitalWrite(greenLed,HIGH);
-  digitalWrite(redLed,HIGH);
+  digitalWrite(greenLed,LOW);
+  digitalWrite(redLed,LOW);
   
   
   lcd.init();                      
@@ -51,35 +57,11 @@ void setup(){
   
 void loop()
 {
-  String s="Dist: ";
-  int dist=getDistance();
-  s=s+String(dist);
-  if(dist>=100)
-  {
-    changeLedText(s,"");
-    maiCauta();
-    capRobot.write(angle);
-    delay(800);
-  }
-
-  else
-  {
-  int distance=getDistance();
-  delay(800);
-  String recived="";
-  if (irrecv.decode())
-  {
-  recived=String(irrecv.decodedIRData.decodedRawData, HEX);
-  irrecv.resume(); 
-  }
-  
-  changeLedText(s,recived);
-  }
-   
+  showMenu();
 }
 
 
-
+//############################# Distanta 
 
 int getDistance()
 {
@@ -97,6 +79,11 @@ int getDistance()
   // Displays the distance on the Serial Monitor
   return distance;
 }
+
+//############################# 
+
+
+//############################# mai cauta
 
 void maiCauta()
 {
@@ -124,6 +111,9 @@ void maiCauta()
     return;
   }
 }
+//############################# 
+
+//############################# Change led text
 
 void changeLedText(String text1, String text2)
 {
@@ -144,6 +134,194 @@ void changeLedText(String text1, String text2)
   lcd.setCursor(xPos,1);
   lcd.print(text2);
   line2=text2;
- }    
-   
+ }      
 }
+
+//############################# 
+
+//############################# input telecomanda
+String getInput()
+{
+  String recived="";
+  delay(100);
+  if (irrecv.decode())
+  {
+  recived=String(irrecv.decodedIRData.decodedRawData, HEX);
+  irrecv.resume(); 
+  }
+  Serial.println(recived);
+  return recived;
+}
+//############################# 
+
+
+//############################# distanta cap
+
+void pastreazaDistanta()
+{
+  while(1)
+  {
+  if(getInput()=="ad52ff00")
+  break;
+  
+  String s="Dist: ";
+  int dist=getDistance();
+  s=s+String(dist);
+  delay(100);
+  if(dist>=100)
+  {
+    changeLedText(s,"Distanta ok");
+    maiCauta();
+    capRobot.write(angle);
+    aprindeVerde();
+    delay(500);
+  }
+
+  else
+  {
+  int distance=getDistance();
+  
+  changeLedText(s,"Pastreada dist.");
+  aprindeRosu();
+  delay(500);
+  
+  }
+  }
+  showMenu();
+}
+
+//############################# 
+
+
+//############################# 
+
+void showMenu()
+{
+  inchideLeduri();
+ if(option==0)
+ {
+    String input=getInput();
+   changeLedText("Pastreaza","Distanta >");
+   if(input=="e31cff00")
+   pastreazaDistanta();
+
+   if(input=="a55aff00")
+   {
+   option++;
+   }
+ }
+
+  if(option==1)
+  {
+   changeLedText("Pastreaza","< Linistea >");
+   if(getInput()=="e31cff00")
+   pastreazaLinistea();
+
+   if(getInput()=="a55aff00")
+   {
+   option++;
+   }
+
+   if(getInput()=="f708ff00")
+   {
+    option--;
+   }
+  }
+
+  if(option==2)
+  {
+    changeLedText("Joc","< Reflex >");
+    if(getInput()=="e31cff00")
+    jocReflex();
+   
+  // if(getInput()=="a55aff00")
+  // option++;
+
+   if(getInput()=="f708ff00")
+   {
+    option--;
+   }
+  }
+}
+
+//############################# 
+
+//############################# Joc reflex
+
+void jocReflex()
+{
+  changeLedText("Wait","Till green");
+  int x=random(2000,10000);
+  aprindeRosu();
+  delay(x);
+  aprindeVerde();
+  changeLedText("Push!!!","");
+  int i=0;
+  int j=0;
+  while(1)
+  {
+    buttonState = digitalRead(buttonPin);
+    if(buttonState == LOW)
+    {
+      break;
+    }
+    j++;
+    if(j==1000)
+    {
+    j=0;
+    i++;
+    }
+  }
+  String scor=String(i);
+  changeLedText("Scor",scor);
+  delay(2000);
+  showMenu();
+}
+
+//############################# 
+
+//#############################  LINISTE
+void pastreazaLinistea()
+{
+  while(1)
+  {
+  if(getInput()=="ad52ff00")
+  break;
+  int var=analogRead(micPin);
+  delay(200);
+  if(var>=68)
+  {
+  aprindeRosu();
+  changeLedText("E galagie!",String(var));
+  }
+  else {
+  aprindeVerde();
+   changeLedText("E liniste!",String(var));}
+  
+  }
+  showMenu(); 
+}
+//############################# 
+
+
+//############################# led control
+void aprindeRosu()
+{
+  digitalWrite(redLed,HIGH);
+  digitalWrite(greenLed,LOW);
+}
+
+
+void aprindeVerde()
+{
+  digitalWrite(greenLed,HIGH);
+  digitalWrite(redLed,LOW);
+}
+
+void inchideLeduri()
+{
+  digitalWrite(greenLed,LOW);
+  digitalWrite(redLed,LOW);
+}
+
+//############################# 
